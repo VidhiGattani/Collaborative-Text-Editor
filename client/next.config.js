a5f3759f752da2d486c4c79e6738fa03e839946b
VidhiGattani <gattanividhi09@gmail.com>
2026-04-04 23:10:03 +0530

    remove invalid turbopack config

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,6 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['bcryptjs', 'pg'],
-  webpack: (config) => {
-    config.externals = config.externals || [];
-    config.externals.push({
-      'bcryptjs': 'commonjs bcryptjs',
-      'pg': 'commonjs pg'
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
